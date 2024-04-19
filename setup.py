da02176c52ae94d3883f2269a41f004cd6bb0974
Mr-Major-K <161507882+mr-major-k-programmer@users.noreply.github.com>
2024-04-19 15:26:12 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='loggingpython',
-    version='1.4.1',
+    version='1.4.2',
     description='Loggingpython is a Python package that provides a simple and\
  extensible way to integrate logging into your applications. The package\
  starts with a simple logger and can be extended with additional functions to\
