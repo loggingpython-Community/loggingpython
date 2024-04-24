34a7df58312b961c3b9af40c2f3b9be6e16fc7a1
Mr-Major-K <161507882+mr-major-k-programmer@users.noreply.github.com>
2024-04-24 13:36:38 +0200

    Create conf.py

---
+++ doc_sphinx/conf.py
@@ -0,0 +1,41 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'loggingpython'
+copyright = '2024, Mr-Major-K'
+author = 'Mr-Major-K'
+
+version = '1.4'
+release = '1.4.2'
+
+language = 'en'
+
+html_theme = 'python_docs_theme'
+
+html_theme_options = {
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+}
+
+html_static_path = ['_static']
+
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store'
+]
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
