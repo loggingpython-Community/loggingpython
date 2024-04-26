b3eda65327f6c0e0717b1529f042a63ba6380015
Mr-Major-K <161507882+mr-major-k-programmer@users.noreply.github.com>
2024-04-26 22:05:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=['colorama', 'pandas'],
     license='MIT',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
