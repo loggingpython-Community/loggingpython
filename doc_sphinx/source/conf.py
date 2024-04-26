ff124460bbfc977ff9f381674dd8ae3cd6a40310
Mr-Major-K <161507882+mr-major-k-programmer@users.noreply.github.com>
2024-04-26 21:38:49 +0200

    Update conf.py

--- doc_sphinx/source/conf.py
+++ doc_sphinx/source/conf.py
@@ -1,9 +1,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('/home/runner/work/loggingpython/\
-loggingpython/src'))
-
 
 project = 'loggingpython'
 copyright = '2024, Mr-Major-K'
