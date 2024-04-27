352fa29622fdbd7c01f4a981fd5f53a47583b5ef
Mr-Major-K <161507882+mr-major-k-programmer@users.noreply.github.com>
2024-04-28 00:51:26 +0200

    Update conf.py

--- doc_sphinx/source/conf.py
+++ doc_sphinx/source/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.setrecursionlimit(99999)
+sys.setrecursionlimit(2000)
 
 project = 'loggingpython'
 copyright = '2024, Mr-Major-K'
